03572af790e9ce8da2a8ba8d4e5b32e706213f59
Etienne Lemay <etiennelemay21@gmail.com>
2012-05-25 12:39:28 -0400

    Add login w/ GitHub

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 # App
 gem "sinatra", "~> 1.3.2"
 gem "erubis", "~> 2.7.0"
+gem "omniauth-github"
 
 # Development dependencies
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
+# Gems w/ Bundle
 if defined?(Bundler)
   Bundler.require(:default, ENV['RACK_ENV'])
 end
 
+# Env settings
+if File.exists?(File.expand_path('../environment.rb', __FILE__))
+  require File.expand_path('../environment', __FILE__)
+end
+
 module GitHubNewsFeed
   class App < Sinatra::Base
+    use OmniAuth::Strategies::GitHub, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+
     set :sessions, true
     set :root, File.expand_path('../../app',  __FILE__)
     set :erb, :layout => :'layouts/application'
@@ -13,5 +21,11 @@ module GitHubNewsFeed
     get '/' do
       erb :index
     end
+
+    get '/auth/:provider/callback' do
+      ap request.env['omniauth.auth']
+      redirect '/'
+    end
+
   end
 end
