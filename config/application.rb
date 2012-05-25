0c872fc55ccccf7fa033ce61ce6c6e34b876b915
Etienne Lemay <etiennelemay21@gmail.com>
2012-05-25 17:36:26 -0400

    Autoload lib files

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,13 @@ if defined?(Bundler)
   Bundler.require(:default, ENV['RACK_ENV'])
 end
 
+# Lib
+Dir[File.expand_path('../../lib/*/*.rb', __FILE__)].each do |file|
+  dirname = File.dirname(file)
+  file_basename = File.basename(file, File.extname(file))
+  require "#{dirname}/#{file_basename}"
+end
+
 # Env settings
 if File.exists?(File.expand_path('../environment.rb', __FILE__))
   require File.expand_path('../environment', __FILE__)
