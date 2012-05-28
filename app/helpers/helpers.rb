3d7211433340586f8d9fbcfecd58956fc256ce20
Etienne Lemay <etiennelemay21@gmail.com>
2012-05-27 22:32:45 -0400

    Add date helpers source

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,9 @@
+# http://api.rubyonrails.org/classes/ActionView/Helpers/DateHelper.html#method-i-time_ago_in_words
 def time_ago_in_words(from_time, include_seconds = false)
   distance_of_time_in_words(from_time, Time.now, include_seconds)
 end
 
+# http://api.rubyonrails.org/classes/ActionView/Helpers/DateHelper.html#method-i-distance_of_time_in_words
 def distance_of_time_in_words(from_time, to_time = 0, include_seconds = false, options = {})
   from_time = from_time.to_time if from_time.respond_to?(:to_time)
   to_time = to_time.to_time if to_time.respond_to?(:to_time)
