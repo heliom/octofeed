35c82178f55f54ed8c69a03611a5e3472ebfbd20
Etienne Lemay <etiennelemay21@gmail.com>
2012-05-29 15:48:14 -0400

    Add page navigation

--- config/application.rb
+++ config/application.rb
@@ -37,41 +37,43 @@ module GitHubNewsFeed
     set :root, File.expand_path('../../app',  __FILE__)
     set :erb, :layout => :'layouts/application'
 
-    get '/' do
-      @user = session[:user]
-      @events = []
+    ['/', '/page/:page_number'].each do |path|
+      get path do
+        @user = session[:user]
+        @page_number = params[:page_number].to_i || 1
 
-      if @user
-        # Repos watched
-        repos_uri = URI.parse("https://api.github.com/user/watched?access_token=#{@user[:token]}")
+        if @user
+          # Repos watched
+          repos_uri = URI.parse("https://api.github.com/user/watched?access_token=#{@user[:token]}")
 
-        repos_http = Net::HTTP.new(repos_uri.host, repos_uri.port)
-        repos_http.use_ssl = true
-        repos_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          repos_http = Net::HTTP.new(repos_uri.host, repos_uri.port)
+          repos_http.use_ssl = true
+          repos_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-        repos_request = Net::HTTP::Get.new(repos_uri.request_uri)
-        repos_response = repos_http.request(repos_request)
+          repos_request = Net::HTTP::Get.new(repos_uri.request_uri)
+          repos_response = repos_http.request(repos_request)
 
-        repos = JSON.parse(repos_response.body)
-        watched_repos = []
-        repos.each do |repo|
-          watched_repos << repo['full_name']
+          repos = JSON.parse(repos_response.body)
+          watched_repos = []
+          repos.each do |repo|
+            watched_repos << repo['full_name']
+          end
+
+          # Events
+          uri = URI.parse("https://api.github.com/users/#{@user[:username]}/received_events?access_token=#{@user[:token]}&page=#{@page_number}")
+
+          http = Net::HTTP.new(uri.host, uri.port)
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+          request = Net::HTTP::Get.new(uri.request_uri)
+          response = http.request(request)
+
+          GitHubNewsFeed::EventParser.parse(response.body, watched_repos)
         end
 
-        # Events
-        uri = URI.parse("https://api.github.com/users/#{@user[:username]}/received_events?access_token=#{@user[:token]}")
-
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-        request = Net::HTTP::Get.new(uri.request_uri)
-        response = http.request(request)
-
-        @events = GitHubNewsFeed::EventParser.parse(response.body, watched_repos)
+        erb :index
       end
-
-      erb :index
     end
 
     get '/auth/:provider/callback' do
