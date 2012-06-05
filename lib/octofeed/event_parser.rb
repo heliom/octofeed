18d33a0e3c1c72e3b4e5c41ee3c178cfeeb220ed
Etienne Lemay <etiennelemay21@gmail.com>
2012-06-05 13:42:31 -0400

    List your own repos with the watched repos

--- lib/octofeed/event_parser.rb
+++ lib/octofeed/event_parser.rb
@@ -4,21 +4,27 @@ module OctoFeed
 
     def initialize(raw_json, watched_repos, session)
       @groups = []
+      @session = session
+      @watched_repos = watched_repos
       events = JSON.parse(raw_json)
 
       events.each do |event_json|
         event_classname = "OctoFeed::#{event_json['type']}"
         event_class = event_classname.split('::').inject(Object) { |o,c| o.const_get c }
         if event_class == OctoFeed::FollowEvent || event_class == OctoFeed::PushEvent
-          event = event_class.new(event_json, session)
+          event = event_class.new(event_json, @session)
         else
           event = event_class.new(event_json)
         end
-        event.set_group watched_repos.include?(event.repo[:name])
+        event.set_group is_watching?(event.repo[:name])
 
         group = find_or_create_group(event.group)
         group.add_event event
       end
+    end
+
+    def is_watching?(repo_name)
+      @watched_repos.include?(repo_name) || repo_name.include?(@session[:user][:username])
     end
 
     def find_or_create_group(group_data)
