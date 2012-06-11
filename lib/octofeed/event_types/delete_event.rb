f6f88918d8e40ee47e0d9bb53f3b2fa4aa8e0ff2
Etienne Lemay <etiennelemay21@gmail.com>
2012-06-11 13:48:03 -0400

    Merge delete events with push and create (grouped by ref)

--- lib/octofeed/event_types/delete_event.rb
+++ lib/octofeed/event_types/delete_event.rb
@@ -10,14 +10,28 @@ module OctoFeed
       }
     end
 
-    def print
-      link = case @object[:type]
-             when 'branch', 'tag' then "#{@object[:ref]} at #{gh_link @repo[:name]}"
-             else "#{gh_link @repo[:name]}"
-             end
+    def ref(with_repo=true)
+      case @object[:type]
+      when 'branch', 'tag'
+        if with_repo
+          "#{@object[:ref]} at #{gh_link @repo[:name]}"
+        else
+          %(<a>#{@object[:ref]}</a>)
+        end
+      else "#{gh_link @repo[:name]}"
+      end
+    end
 
+    def print
       super({
-        :title => "#{gh_user_link @actor[:username]} deleted #{@object[:type]} #{link}"
+        :title => "#{gh_user_link @actor[:username]} deleted #{@object[:type]} #{ref}"
+      })
+    end
+
+    def set_repo_group
+      super({
+        :id => "#{@repo[:name]}-commits-#{@object[:ref]}",
+        :title => "#{gh_user_repo_link @repo[:name]} #{extra(ref false)}"
       })
     end
 
