7f59eafe1fed3d53d0a9ac6799da10fb0a1caed3
Etienne Lemay <etiennelemay21@gmail.com>
2012-06-20 15:10:31 -0400

    Fix “a minute ago”

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -29,6 +29,8 @@ def distance_of_time_in_words(from_time, to_time = 0, include_seconds = false, o
     when 0..1
       return distance_in_minutes == 0 ?
              "less than a minute" :
+             distance_in_minutes == 1 ?
+             'a minute' :
              "#{distance_in_minutes} minutes" unless include_seconds
 
       case distance_in_seconds
